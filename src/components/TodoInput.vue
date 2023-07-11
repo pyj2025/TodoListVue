620e962026b2b8e8adaeeca6d9c74e88d1dddb50
Youngjoon Park <pyj2025@gmail.com>
2023-07-11 16:22:50 -0500

    add ConfirmationModal

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,19 +1,28 @@
+<!-- eslint-disable vue/no-deprecated-slot-attribute -->
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" placeholder="Type" v-on:keypress.enter="addTodo">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keypress.enter="addTodo">
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <ConfirmationModal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">할 일을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </ConfirmationModal>
   </div>
 </template>
 
-
-  
 <script>
+import ConfirmationModal from './ConfirmationModal.vue'
+
 export default {
   data() {
     return {
       newTodoItem: '',
+      showModal: false
     }
   },
   methods: {
@@ -22,13 +31,16 @@ export default {
         var value = this.newTodoItem && this.newTodoItem.trim();
 				this.$emit('addTodo', value)
         this.clearInput();
-      } 
+      } else {
+        this.showModal = !this.showModal;
+      }
     },
     clearInput() {
       this.newTodoItem = '';
     }
   },
   components: {
+    ConfirmationModal: ConfirmationModal
   }
 }
 </script>
