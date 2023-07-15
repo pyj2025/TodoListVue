ded6f6029e29ab1998e463f8f7f5b4d60e1d09ce
Youngjoon Park <pyj2025@gmail.com>
2023-07-14 20:48:06 -0500

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "tailwindcss/tailwind.css";
 import App from "./App.vue";
+import store from "./store/storage";
 
-createApp(App).mount("#app");
+createApp(App).use(store).mount("#app");
